2d84543dc2c2562a119a7161b7b23656d25c0631
Alex Archer <alex.archer@okta.com>
2022-07-13 16:11:16 -0400

    initial commit

---
+++ src/components/ArenguForm.js
@@ -0,0 +1,10 @@
+import React from "react"
+import { ArenguForm } from "react-arengu-forms"
+
+const ArenguFormInput = () => (
+  <div>
+    <ArenguForm id="165729067487326848" />
+  </div>
+)
+
+export default ArenguFormInput
