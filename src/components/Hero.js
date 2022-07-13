56c75a78a3de14d95b0ee7ebf86db7bb894abf12
Alex Archer <alex.archer@okta.com>
2022-07-13 16:50:05 -0400

    update branding for healthcare

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,16 +1,16 @@
 import React from "react";
 
-import logo from "../assets/logo.svg";
-
 const Hero = () => (
-  <div className="text-center hero my-5">
-    <img className="mb-3 app-logo" src={logo} alt="React logo" width="120" />
-    <h1 className="mb-4">React.js Sample Project</h1>
+  <div className="text-center my-5">
+    <img className="mb-3" id="hero-background" src="https://i.ibb.co/VqfS6qK/1440x300-banner-lbh-home-page-banner-image.jpg" alt="heathcare-truck" width="120" />
+    <h1 className="mb-4">COVID-19
+      Walk-ins are now welcome.
 
-    <p className="lead">
-      This is a sample application that demonstrates an authentication flow for
-      an SPA, using <a href="https://reactjs.org">React.js</a>
-    </p>
+      Get the latest about updated visitor policies, news, and more!
+
+      <a className="hero-text" target="_blank" href="https://www.lifebridgehealth.org/Coronavirus/Coronavirus.aspx">LEARN MORE</a> 
+    </h1>
+
   </div>
 );
 
